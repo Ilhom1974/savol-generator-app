cc8d7d81a3c47fec19a131db6d1d4227dc14bb4a
Ilhom1974 <51198082+Ilhom1974@users.noreply.github.com>
2026-09-17 14:02:39 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@
 API_KEY_ENV_VAR = "GEMINI_API_KEY"
 
 # Biz foydalanayotgan Google Gemini modelining nomi
-MODEL_NAME = "models/gemini-flash-latest"
+# MODEL_NAME = "models/gemini-flash-latest"
+MODEL_NAME = "gemini-2.5-flash"
 
 # Fayl yuklash uchun ruxsat etilgan kengaytmalar (kelajakda qo'shish uchun)
 # ALLOWED_EXTENSIONS = {'txt', 'docx', 'pdf', 'pptx'}
